7802e1e9a90207bda9fc6c716adbd24e474d1b1b
msimonds <msimonds@oberlin.edu>
2016-12-07 18:16:22 -0500

    Fixed instances, still missing labels tho

--- src/FinalProj.java
+++ src/FinalProj.java
@@ -40,10 +40,10 @@ public class FinalProj {
 			int fuckme = 9;
 			while (file.hasNextLine()){
 				String[] lineParts = line.split(",");				
-				String title = lineParts[0].substring(0,lineParts[0].length());
+				String title = lineParts[0].substring(1,lineParts[0].length()-1);
 				//also save notes to determine if part of series later
-				String notes = lineParts[lineParts.length-1].substring(0,lineParts[lineParts.length-1].length());
-				String director = lineParts[1].substring(0,lineParts[1].length());
+				String notes = lineParts[lineParts.length-1].substring(1,lineParts[lineParts.length-1].length()-1);
+				String director = lineParts[1].substring(1,lineParts[1].length()-1);
 				//System.out.println(notes + " ** "+director);
 				JSONObject json = OMDB(title, year);
 				Double tomato=-1.0;
@@ -64,7 +64,7 @@ public class FinalProj {
 				}
 				line = file.nextLine();
 				fuckme=fuckme+1;
-				if(fuckme==15){
+				if(fuckme==1000){
 					movies.close();
 				}
 			}
@@ -147,14 +147,24 @@ public class FinalProj {
 	public static String getData(JSONObject movie, String sequel) throws JSONException{
 		StringBuffer str = new StringBuffer();
 		str = str.append(movie.get("Director").toString()+",");
+		
 		String[] writers = movie.get("Writer").toString().split(",");
 		if(writers.length>1){
 			str = str.append(stripParens(writers[0]) + ","+stripParens(writers[1])+",");
 		} else{
 			str=str.append(stripParens(writers[0])+", ,");
 		}
+		
 		String[] allActors = movie.get("Actors").toString().split(",");
-		str = str.append(stripParens(allActors[0]) + ","+stripParens(allActors[1])+","+stripParens(allActors[2])+",");
+		if(allActors.length>2){
+			str = str.append(stripParens(allActors[0]) + ","+stripParens(allActors[1])+","+stripParens(allActors[2])+",");
+		} else if(allActors.length==2){
+			str = str.append(stripParens(allActors[0]) + ","+stripParens(allActors[1])+", ,");
+		} else if(allActors.length==1){
+			str = str.append(stripParens(allActors[0]) + ", , ,");
+		} else{
+			str = str.append(", , , ,");
+		}
 		
 		String awards[] = movie.get("Awards").toString().split(" ");
 		int numAwards = 0;
@@ -163,16 +173,19 @@ public class FinalProj {
 				numAwards = numAwards + Integer.parseInt(s);
 			}
 		}
-		str = str.append(numAwards+","+sequel+",");
+		
+		str = str.append(numAwards+","+sequel+",");		
 		str = str.append(movie.get("tomatoMeter")+","+movie.get("tomatoUserMeter")+",");
+		
 		String boxof = movie.get("BoxOffice").toString().substring(1);
-		if(Character.isDigit(boxof.charAt(0))){
-			 str = str.append(Integer.parseInt(boxof)+",");
+		System.out.println(boxof);
+		if(Character.isDigit(boxof.charAt(0))){			 
+			 str = str.append((Double.parseDouble(boxof.replaceAll(",", ""))+",").toString());
 		}else{
 			str = str.append(" ,");
 		}
-		String[] genres = movie.get("Genre").toString().split(",");
-		//we want to add up to 3 genres if theres less than three then we want to pad with blanks spaces
+		
+		String[] genres = movie.get("Genre").toString().split(",");		
 		int count =0;
 		for(String x : genres){
 			if(count<3){
@@ -183,10 +196,7 @@ public class FinalProj {
 		while(count<3){
 			str.append(" " +",");
 			count=count+1;
-		}
-		
-		
-		
+		}		
 		return str.toString();
 	}
 	
@@ -195,7 +205,13 @@ public class FinalProj {
 		String newS=str;
 		if(index>=0){
 			newS = str.substring(0, index);
-		}		
+		}
+		if(newS.charAt(0)==' '){
+			newS = newS.substring(1);
+		}
+		if(newS.charAt(newS.length()-1)==' '){
+			newS = newS.substring(0, newS.length()-1);
+		}
 		return newS;
 	}
 
